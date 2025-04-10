f95d9f9aa95f533642d4e1e8b3ee7b96a386c764
Stuart Fletcher <stuart.fletcher@gmail.com>
2025-04-10 10:34:14 -0700

    update main.go

--- main.go
+++ main.go
@@ -11,47 +11,65 @@ import (
 )
 
 func main() {
-	from := flag.String("from", "", "path to the input djot file (empty or '-' for stdin)")
-	to := flag.String("to", "", "path to the output html file (empty or '-' for stdout)")
-	overwrite := flag.Bool("overwrite", false, "overwrite output html file")
+	os.Exit(run())
+}
+
+// TODO
+// - Files aren't cleaned up when errors are hit.
+
+func run() int {
+	var (
+		from      = flag.String("from", "", "path to the input djot file (empty or '-' for stdin)")
+		to        = flag.String("to", "", "path to the output html file (empty or '-' for stdout)")
+		overwrite = flag.Bool("overwrite", false, "overwrite output html file")
+
+		in  io.Reader
+		out io.Writer
+	)
 	flag.Parse()
 
-	var inReader io.Reader
-	var outWriter io.Writer
 	if *from == "" || *from == "-" {
-		inReader = os.Stdin
+		in = os.Stdin
 	} else {
 		f, err := os.Open(*from)
 		if err != nil {
-			log.Fatalf("failed to open input file %v: %v", *from, err)
+			log.Printf("failed to open input file %v: %v", *from, err)
+			return 1
 		}
-		inReader = f
+		in = f
+		defer f.Close()
 	}
 	if *to == "" || *to == "-" {
-		outWriter = os.Stdout
+		out = os.Stdout
 	} else {
 		flags := os.O_CREATE | os.O_WRONLY | os.O_TRUNC
 		if !*overwrite {
 			flags |= os.O_EXCL
 		}
-		f, err := os.OpenFile(*to, flags, 0660)
+		f, err := os.OpenFile(*to, flags, 0640)
 		if err != nil {
-			log.Fatalf("failed to open output file %v: %v", *to, err)
+			log.Printf("failed to open output file %v: %v", *to, err)
+			return 1
 		}
-		outWriter = f
+		defer f.Close()
+		out = f
 	}
-	input, err := io.ReadAll(inReader)
+	input, err := io.ReadAll(in)
 	if err != nil {
-		log.Fatalf("failed to read input file %v: %v", *from, err)
+		log.Printf("failed to read input file %v: %v", *from, err)
+		return 1
 	}
 	ast := djot_parser.BuildDjotAst(input)
 	context := djot_parser.NewConversionContext("html", djot_parser.DefaultConversionRegistry)
 	html := []byte(context.ConvertDjotToHtml(&html_writer.HtmlWriter{}, ast...))
 	for len(html) > 0 {
-		n, err := outWriter.Write(html)
+		n, err := out.Write(html)
 		if err != nil {
-			log.Fatalf("failed to write output file %v: %v", *to, err)
+			log.Printf("failed to write output file %v: %v", *to, err)
+			return 1
 		}
 		html = html[n:]
 	}
+
+	return 0
 }
